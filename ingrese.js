0e47cdc284bc5852780ed4bb68f81c63b8bc9b6c
IsaacFerreto <ferreto.isaac.com>
2024-06-03 15:31:37 -0600

    forWithAppendChildToaddElements

--- ingrese.js
+++ ingrese.js
@@ -2,8 +2,11 @@
 import { obtenerDatos } from "./obtener";
 import { ingresoTexto } from "./src";
 
+//Main pourpuse of this function is to submmit informmation to an object,
+// that object is going to added to an array and the array to the local host and to the screen
 export async function darDatos() {
     try {
+        //Object creation
       let tarea =
       {
         id:Date.now(),
@@ -12,7 +15,7 @@ export async function darDatos() {
       }
 
       
-      
+      //this part is going to submit method POST so we can add with fecht the information to the local host
       const respuesta = await fetch("http://localhost:3000/api/task",{
         method: "POST",
         headers: {
@@ -23,6 +26,13 @@ export async function darDatos() {
       console.log(`Se agrego satisfactoriamente la tarea ${tarea.nombre}`);  
       const datos = await respuesta.json()
       console.log(datos);
+// for to add elements to the screen
+      datos.forEach(element => {
+        let li= document.createElement('p')
+         li.innerHTML=element.nombre
+         let body = document.body
+         body.appendChild(li)
+     });
     } catch (error) {
         console.error(error);
     }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import { obtenerDatos } from "../obtener";
 import { darDatos } from "../ingrese";
-
-export let ingresoTexto=document.getElementById("texto").value;
+//calling GET to be dure that information is always going to be updated
+obtenerDatos();
+//sending input to module where the input is going to be read
+export let ingresoTexto=document.getElementById("texto");
+// calling bottom from HTML to activate event listener (bottom should add a new object))
 let agregar=document.getElementById("agregar");
 
- 
-obtenerDatos();
+ //listener for function that is being run in 'ingrese.js' where its funtionality is explained
 agregar.addEventListener('click', darDatos)
