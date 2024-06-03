9bc04f9a231ca6005555d830da40501748afd272
IsaacFerreto <ferreto.isaac.com>
2024-06-03 10:29:40 -0600

    BasicHTMLEstructureAdded

---
+++ ingrese.js
@@ -0,0 +1,24 @@
+//POST
+async function darDatos() {
+    try {
+      let tarea =
+      {
+        id:Date.now(),
+        nombre:input.value,
+        estado:false
+      }
+      const respuesta = await fetch(" ",{
+        method: "POST",
+        headers: {
+            "Content-type": "application/json; charset=UTF-8"
+          },
+          body: JSON.stringify(tarea)
+      })
+      console.log(`Se agrego satisfactoriamente la tarea ${tarea.nombre}`);  
+      const datos = await respuesta.json()
+      console.log(datos);
+    } catch (error) {
+        console.error(error);
+    }
+    //line to put the get function
+}
